acdaa4f26ad71d1609d70b8ba09c469ca49c0d25
yusufhadi <hadiyusuf889@gmail.com>
2022-05-14 23:03:51 +0800

    update sending email afterCheckout

--- resources/views/emails/checkout/afterCheckout.blade.php
+++ resources/views/emails/checkout/afterCheckout.blade.php
@@ -6,7 +6,7 @@
     Thank you for register on <b>{{ $checkout->Camp->title }}</b>, please see payment instruction by click the button
     below.
 
-    @component('mail::button', ['url' => route('user.checkout.invoice', $checkout->id)])
+    @component('mail::button', ['url' => route('dashboard')])
         Get Invoice
     @endcomponent
 
